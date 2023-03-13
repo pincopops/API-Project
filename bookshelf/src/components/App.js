b88cfdcbdb841460fe28cef920584def5993a88f
Elio Scaletta <elioscal@hotmail.it>
2023-03-13 16:16:51 +0100

    Creating react app and give it the first structure.

---
+++ bookshelf/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../components_scss/main_scss/_Style.scss";
+
+function App(){
+    return(
+        <>
+        <h1>React app</h1>
+        </>
+    )
+}
+
+export default App;
